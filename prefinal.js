82599042c1f43bd79923f741d78b08113879f894
toran billups <toranb@gmail.com>
2014-07-03 14:26:02 -0500

    updated prefinal to use really long path

--- prefinal.js
+++ prefinal.js
@@ -3,7 +3,7 @@ var AsyncComponent = require('./component');
 
 var PreFinal = React.createClass({displayName: 'PreFinal',
     mixins: [AsyncComponent],
-    bundle: './final',
+    bundle: 'jspm_packages/github/toranb/toranb-react-jspm-view@0.1.2/final',
     preRender: function() {
       return React.DOM.div(null, "Loading final...")
     }
