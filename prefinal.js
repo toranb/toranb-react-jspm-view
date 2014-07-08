826b8de733785ce1fc19c12aff0b7533fba4130e
toran billups <toranb@gmail.com>
2014-07-08 12:58:57 -0500

    killed the complexity of a mixin 0.1.6

--- prefinal.js
+++ prefinal.js
@@ -1,9 +1,34 @@
 var React = require('react');
-var AsyncComponent = require('./component');
 
 var PreFinal = React.createClass({displayName: 'PreFinal',
-    mixins: [AsyncComponent],
-    bundle: 'github:toranb/toranb-react-jspm-view@0.1.5/final',
+    loadedComponent: null,
+    load: function() {
+      var self = this;
+      if (this.constructor.loadedComponent) {
+        return;
+      }
+      System.import("github:toranb/toranb-react-jspm-view@0.1.6/final")
+      .then(function(es6_module) {
+        return es6_module.default;
+      })
+      .then(function(component) {
+        self.constructor.loadedComponent = component;
+        self.forceUpdate();
+      })
+      .catch(function(e) {
+        console.log("error loading the final module");
+        console.log(e);
+      });
+    },
+
+    componentDidMount: function() {
+      setTimeout(this.load, 500);
+    },
+
+    render: function() {
+      var component = this.constructor.loadedComponent;
+      return component ? component(this.props) : this.preRender();
+    },
     preRender: function() {
       return React.DOM.div(null, "Loading final...")
     }
